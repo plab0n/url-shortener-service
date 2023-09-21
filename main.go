ba80d2e10e46a7c753afbe91a5022cf5a9ee5e0e
plab0n <plaboniubat16@gmail.com>
2023-09-21 20:47:36 +0600

    Added viper for config management

--- main.go
+++ main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"github.com/spf13/viper"
 	"url-shortener-service/api"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	viper.AddConfigPath("./configs")
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 	r := gin.Default()
 	api.SetupRoutes(r)
 	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	port := viper.GetString("httpPort")
+	r.Run(port)
 }
