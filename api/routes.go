55d6a8ea65a3815c2187b77f273676e33b546951
plab0n <plaboniubat16@gmail.com>
2023-09-23 20:41:26 +0600

    Route modified

--- api/routes.go
+++ api/routes.go
@@ -9,7 +9,7 @@ import (
 func SetupRoutes(router *gin.Engine) {
 	router.POST("/api/v1/data/shorten", handlers.CreateShortUrl)
 	router.DELETE("/api/v1/data", handlers.DeleteUrlHandler)
-	router.GET(":shortUrl", handlers.RedirectToLongUrl)
+	router.GET("/api/v1/:shortUrl", handlers.RedirectToLongUrl)
 
 	router.POST("/api/v1/auth/register", handlers.RegisterHandler)
 	router.GET("/api/v1/auth/token", handlers.TokenHandler)
